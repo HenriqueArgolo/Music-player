4a6eeefa31b902492f8dc4e7ee55fe16663e4933
HenriqueArgolo <henrique73357@gmail.com>
2024-03-03 21:23:17 -0300

    bottoSheet

--- app/src/main/java/com/henriqueargolo/musicappplayer/ui/activities/FullScreenPlayer.kt
+++ app/src/main/java/com/henriqueargolo/musicappplayer/ui/activities/FullScreenPlayer.kt
@@ -2,11 +2,18 @@ package com.henriqueargolo.musicappplayer.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.henriqueargolo.musicappplayer.R
 
 class FullScreenPlayer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_full_screen_player)
+        val sheetPlaylist = findViewById<FrameLayout>(R.id.sheet_playlist)
+        BottomSheetBehavior.from(sheetPlaylist).apply {
+            peekHeight = 200
+            this.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
     }
 }
